b8539148439a81cbe6f537da0d66d8fa20cce62d
kyaloeriq <kyaloeriq@gmail.com>
2023-08-19 04:49:55 -0700

    function that produces output according to a format

--- printf.c
+++ printf.c
@@ -52,11 +52,11 @@ int _printf(const char *format, ...)
 				printf("%o", num1);
 				printed_chars += 4;
 			}
-			else if (*fmt_ptr == 'x' && *fmt_ptr == 'X')
+			else if (*fmt_ptr == 'x' || *fmt_ptr == 'X')
 			{
 				num1 = va_arg(args, unsigned int);
-				printf("%x,%X", num1);
-				printed_chars += 4; /* Number of characters printed */
+				printf("%x,%X", num1, num1);
+				printed_chars += 16; /* Number of characters printed */
 			}
 			else if (*fmt_ptr == 'c')
 			{
