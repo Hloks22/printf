83edd2ef6e4752e4c4465f59a17b2d22667f03d5
kyaloeriq <kyaloeriq@gmail.com>
2023-08-21 23:32:48 -0700

    prints an argument based on its type

--- handle_print.c
+++ handle_print.c
@@ -1,15 +1,15 @@
 #include "main.h"
-
+/* project by Daniel and Eric */
 /**
- * handle_print - It Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
+ * handle_print - prints an argument based on its type
+ * @fmt: formatted string in which to print the arguments.
+ * @list: list of arguments to be printed.
+ * @ind: ind
+ * @buffer: buffer array to handle print.
+ * @flags: calculates active flags
  * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
+ * @precision: precision specification
+ * @size: size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
